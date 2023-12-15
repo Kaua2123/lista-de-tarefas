69ffedc44c9f69c6aa67e2e6cc7965db00695e69
Kaua2123 <kauantnetsoares@gmail.com>
2023-12-15 08:21:39 -0300

    Progresso na adição e exibição de tarefas

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 
-(function() {
+(function () {
 
     const addTarefas = document.querySelector('.add-tarefas');
     const btnAddTarefas = document.querySelector('.btn-add-tarefas');
     const tarefas = document.querySelector('.tarefas') // ul 
+    const arrayDeTarefas = [];
 
-    btnAddTarefas.addEventListener('click', (e) => {
+
+    btnAddTarefas.addEventListener('click', () => {
         criaTarefa();
     })
 
@@ -14,21 +16,42 @@
             criaTarefa();
         }
     })
-    
+
     const criaLi = () => {
         const li = document.createElement("li");
         return li;
     }
 
+    const criaBotao = () => {
+        const botao = document.createElement("button");
+        botao.classList.add("delete-tarefa");
+        return botao;
+    }
+
     const criaTarefa = () => {
-        localStorage.setItem("tarefa", addTarefas.value);
+        arrayDeTarefas.push(addTarefas.value)
+        localStorage.setItem("tarefas", arrayDeTarefas)
+
         exibeTarefa();
     }
 
+    const getTarefa = () => {
+        const storage = localStorage.getItem("tarefas").split(',');
+        const storageArray = Array.from(storage)
+        return storageArray;
+    }
+
     const exibeTarefa = () => {
-        const li = criaLi();
-        li.innerText = localStorage.getItem("tarefa");
-        tarefas.appendChild(li) 
+
+        const storageArray = getTarefa();
+        console.log(storageArray);
+
+        for (let i = 0; i < storageArray.length; i++) {
+            const li = criaLi();
+            li.innerText = storageArray[i];
+            tarefas.appendChild(li);
+        }
+
     }
 
     exibeTarefa();
